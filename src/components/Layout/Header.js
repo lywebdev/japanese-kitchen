561880e27a5b5c4b187f01076c2028138b70d7b0
Leonid Kulik <lkwebdev@mail.ru>
2023-11-21 03:41:34 +0300

    JK#1: реализация шапки (115)

---
+++ src/components/Layout/Header.js
@@ -0,0 +1,17 @@
+import React from "react";
+import sushiImage from '../../assets/sushi.jpg';
+import styles from './Header.module.css';
+
+const Header = (props) => {
+    return <React.Fragment>
+        <header className={styles.header}>
+            <h1>Japanese Kitchen</h1>
+            <button>Cart</button>
+        </header>
+        <div>
+            <img src={sushiImage} alt="Japanese Kitchen" className={styles['main-image']}/>
+        </div>
+    </React.Fragment>;
+}
+
+export default Header;
