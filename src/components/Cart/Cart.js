1068d8034d07233556de6773ac971ffae9c42c58
Leonid Kulik <lkwebdev@mail.ru>
2023-12-10 19:32:36 +0100

    JK: ~

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -9,11 +9,26 @@ const Cart = (props) => {
 
     const totalAmount = `$${cartContext.totalAmount.toFixed(2)}`;
     const hasItems = cartContext.items.length > 0;
+    
+    const removeCartItemHandler = (id) => {
+
+    }
+
+    const addCartItemHandler = (item) => {
+
+    }
 
     const cartItems = <ul className={styles['cart-items']}>{[
         {id: 'm1', name: 'Sushi', amount: 2, price: 11}
     ].map((item) => (
-            <CartItem key={props.id} name={item.name} amount={item.amount} />
+            <CartItem
+                key={props.id}
+                name={item.name}
+                amount={item.amount}
+                price={item.price}
+                onAdd={addCartItemHandler.bind(null, item)}
+                onRemove={removeCartItemHandler.bind(null, item.id)}
+            />
         ))}
     </ul>;
 
